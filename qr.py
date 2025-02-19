fdbd0fb3c3dac9034d4caf1f1807eea19c934e22
eduar suarez <eduarjosesuarezgraterol@gmail.com>
2025-02-19 11:19:39 -0400

    codigoQR

---
+++ qr.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# El enlace que quieres que el código QR dirija
+url = 'https://juegowediujoo.netlify.app/'
+
+# Crear un objeto QRCode
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+
+# Agregar el enlace al objeto QRCode
+qr.add_data(url)
+qr.make(fit=True)
+
+# Crear una imagen del QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Guardar la imagen en un archivo
+img.save("codigo_qr.png")
